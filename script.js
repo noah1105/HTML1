cc999ac2f0b8cb5f73a73a9ce05501ac9d6dc251
noah1105 <100046064+noah1105@users.noreply.github.com>
2024-01-31 23:11:03 +0000

    Updates

---
+++ script.js
@@ -0,0 +1,9 @@
+.image-container img,
+.image-container a {  /* Style both images and links within the container */
+  width: 250px;
+  height: 100px;
+  margin: 10px;
+  display: block;  /* Ensure links are block-level */
+  text-align: center;  /* Center link text, if desired */
+  box-sizing: border-box;  /* Include padding and borders in dimensions */
+}
