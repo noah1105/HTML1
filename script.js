dbbede2aced06de17a8c638f8a93da6284236df1
noah1105 <100046064+noah1105@users.noreply.github.com>
2024-03-20 22:11:47 +0000

    The updated Code

--- script.js
+++ script.js
@@ -38,4 +38,33 @@ function showSlides(n) {
   }
 
   slides[slideIndex - 1].style.display = "block";
-}  
+}
+
+// This is the new comment
+
+function showPhotos() {
+  console.log("This is a photo!");
+
+  const outputContainer = document.createElement("div");
+  outputContainer.textContent = "This is a photo!";
+  document.body.appendChild(outputContainer);
+}
+
+const button = document.createElement("button");
+button.textContent = "Photos";
+button.addEventListener("click", showPhotos);
+
+document.body.appendChild(button);
+
+// This is the new comment
+
+function complexConditionEvaluation(value1, value2, value3) {
+  if ((value1 > 5 && value2 < 10) || value3 === "example") {
+    console.log("Complex condition met!");
+  } else {
+    console.log("New update.");
+  }
+}
+
+complexConditionEvaluation(7, 8, "example");
+
